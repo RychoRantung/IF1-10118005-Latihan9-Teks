1fb156e8c036246e88f3e098a3c2eedecea5a44f
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:38:55 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan9-Teks/src/if1/pkg10118005/latihan9/teks/IF110118005Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan9.teks;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program untuk menampilkan teks
+ */
+public class IF110118005Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" +  newline + "char: " + tab + "berisi " + c);
+        
+    }
+    
+}
